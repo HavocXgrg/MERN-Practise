896a9595e0cc7b1a3691b60a312a13e7c0c34dbb
bishwa <108havocx108@gmail.com>
2024-07-03 19:28:33 +0545

    user schema and model with mongoose

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,12 +1,16 @@
+const mongoose = require('mongoose');
+
 const connection = async() => {
     
     //handling promise with async and await
     const isConnected = await mongoose.connect('mongodb://127.0.0.1:27017/test');
     if(isConnected){
-        console.log("connection success")
+        console.log("mongoDb connection success")
     } else {
-        console.log("connection failed")
+        console.log("mongoDb connection failed")
         
     }
 }
 connection()
+
+module.exports = connection
--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const connection = require('./db/connection')
+const Users = require('./model/users')
 
 app.use(express.json()) // to change body(objects prolly) coming from express into json.
 
-const mongoose = require('mongoose');
-
-const connection = async() => {
-    
-    //handling promise with async and await
-    const isConnected = await mongoose.connect('mongodb://127.0.0.1:27017/test');
-    if(isConnected){
-        console.log("connection success")
-    } else {
-        console.log("connection failed")
-        
-    }
-}
-connection()
-
 app.post('/register', (req, res) => {
-    
-    
+    Users.create(req.body)
+    res.send("collection created")
 })
 
 
