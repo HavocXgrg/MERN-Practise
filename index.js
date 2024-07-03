cbb7c54c471939d542dd8618f72fb1550c2ebb00
bishwa <108havocx108@gmail.com>
2024-07-03 17:45:12 +0545

    handling promise with async and await

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const port = 5000
 
 app.use(express.json()) // to change body(objects prolly) coming from express into json.
 
+const mongoose = require('mongoose');
+
+const connection = async() => {
+    const isConnected = await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+    if(isConnected){
+        console.log("connection success")
+    } else {
+        console.log("connection failed")
+        
+    }
+}
+connection()
 
 app.post('/register', (req, res) => {
+    
     
 })
 
