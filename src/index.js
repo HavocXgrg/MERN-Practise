f1b0c5ec84c507c807d48df03b7e2e2452e36912
bishwa <108havocx108@gmail.com>
2024-07-04 21:48:16 +0545

    CRUD using post, get and delete

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
-const port = 5000
+const port = process.env.PORT
 const connection = require('./db/connection')
 const Users = require('./model/users')
 
 app.use(express.json()) // to change body(objects prolly) coming from express into json.
 
-app.post('/register', (req, res) => {
-    Users.create(req.body)
+//POST method where user data in postman(register page) is sent to db
+app.post('/register', async (req, res) => {
+    await Users.create(req.body)
     res.send("collection created")
+})
+
+//GET method to get users data in list
+app.get('/users', async (req,res) => {
+  const list = await Users.find()
+  if(list) res.json({list})
+})
+//DELETE method where a targeted id can be deleted after an id is given after /users/..... in postman
+app.delete('/users/:id', async (req,res) => {
+  const result = await Users.findByIdAndDelete(req.params.id)
+  if(result){
+    res.json({msg:`ID ${req.params.id} deleted successfully`})
+  }
 })
 
 
